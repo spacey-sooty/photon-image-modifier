d1f734e1f69c785fd7e11925c16bcbc1940d377e
Matt <matthew.morley.ca@gmail.com>
2023-12-19 19:43:04 -0500

    Build for LL too

---
+++ install_limelight.sh
@@ -0,0 +1,11 @@
+# Run normal photon instarller
+
+cd /tmp 
+wget https://git.io/JJrEP -O install.sh
+chmod +x install.sh
+sudo ./install.sh
+
+# and edit boot partition
+
+find /boot/
+cat /boot/config.txt
