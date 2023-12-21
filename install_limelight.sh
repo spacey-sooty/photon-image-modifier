83594509cbf76f132ebc8869b8b44b6d531d4ee1
Matt <matthew.morley.ca@gmail.com>
2023-12-21 11:11:49 -0800

    Install all the things

--- install_limelight.sh
+++ install_limelight.sh
@@ -5,7 +5,26 @@ wget https://git.io/JJrEP -O install.sh
 chmod +x install.sh
 sudo ./install.sh
 
+sudo apt-get install -y pigpiod pigpio device-tree-compiler dhcpcd5 network-manager net-tools
+
 # and edit boot partition
 
-find /boot/
-cat /boot/config.txt
+install -m 644 limelight/config.txt /boot/
+install -m 644 userconf.txt /boot/
+
+dtc -O dtb limelight/gloworm-dt.dts -o /boot/dt-blob.bin
+
+# Kill wifi and other networking things
+install -v -m 644 files/wait.conf /etc/systemd/system/dhcpcd.service.d/
+install -v files/rpi-blacklist.conf /etc/modprobe.d/blacklist.conf
+
+# Remove extra packages too
+
+apt-get purge -y python3 gdb gcc g++ linux-headers* libgcc*-dev *qt* wpasupplicant wireless-tools firmware-atheros firmware-brcm80211 firmware-libertas firmware-misc-nonfree firmware-realtek raspberrypi-net-mods device-tree-compiler
+apt-get autoremove -y
+
+rm -rf /var/lib/apt/lists/*
+apt-get clean
+
+rm -rf /usr/share/doc
+rm -rf /usr/share/locale/
--- install_pi.sh
+++ install_pi.sh
@@ -5,10 +5,24 @@ wget https://git.io/JJrEP -O install.sh
 chmod +x install.sh
 sudo ./install.sh
 
-# and edit boot partition
+sudo apt-get install -y pigpiod pigpio device-tree-compiler dhcpcd5 network-manager net-tools
 
-find /boot/
-cat /boot/config.txt
+# and edit boot partition
 
 install -m 644 config.txt /boot/
 install -m 644 userconf.txt /boot/
+
+# Kill wifi and other networking things
+install -v -m 644 files/wait.conf /etc/systemd/system/dhcpcd.service.d/
+install -v files/rpi-blacklist.conf /etc/modprobe.d/blacklist.conf
+
+# Remove extra packages too
+
+apt-get purge -y python3 gdb gcc g++ linux-headers* libgcc*-dev *qt*
+apt-get autoremove -y
+
+rm -rf /var/lib/apt/lists/*
+apt-get clean
+
+rm -rf /usr/share/doc
+rm -rf /usr/share/locale/
