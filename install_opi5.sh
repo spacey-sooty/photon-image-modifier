e88ff4b1ca50467e7982a1454df0cdc791f04826
Matt <matthew.morley.ca@gmail.com>
2023-12-23 07:46:01 -0800

    Create orange pi script

---
+++ install_opi5.sh
@@ -0,0 +1,18 @@
+cd /tmp 
+wget https://git.io/JJrEP -O install.sh
+chmod +x install.sh
+
+sed -i 's/# AllowCPUs=4-7/AllowCPUs=4-7/g' install.sh
+
+./install.sh
+
+# Remove extra packages too
+
+apt-get purge -y python3 gdb gcc g++ linux-headers* libgcc*-dev *qt*
+apt-get autoremove -y
+
+rm -rf /var/lib/apt/lists/*
+apt-get clean
+
+rm -rf /usr/share/doc
+rm -rf /usr/share/locale/
