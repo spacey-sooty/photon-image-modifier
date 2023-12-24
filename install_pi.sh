4096515ab92bafdbee1477a8cfa40e6390e18fb0
Matt <matthew.morley.ca@gmail.com>
2023-12-24 14:03:43 -0800

    test things

--- install_pi.sh
+++ install_pi.sh
@@ -1,14 +1,16 @@
 # Run normal photon instarller
 
+pushd .
 cd /tmp 
 wget https://git.io/JJrEP -O install.sh
 chmod +x install.sh
 ./install.sh
 
+sudo apt-get update
 apt-get install -y pigpiod pigpio device-tree-compiler dhcpcd5 network-manager net-tools
 
 # and edit boot partition
-
+popd
 install -m 644 config.txt /boot/
 install -m 644 userconf.txt /boot/
 
@@ -16,9 +18,12 @@ install -m 644 userconf.txt /boot/
 install -v -m 644 files/wait.conf /etc/systemd/system/dhcpcd.service.d/
 install -v files/rpi-blacklist.conf /etc/modprobe.d/blacklist.conf
 
+# Enable ssh
+systemctl enable ssh
+
 # Remove extra packages too
 
-apt-get purge -y python3 gdb gcc g++ linux-headers* libgcc*-dev *qt*
+# apt-get purge -y python3 gdb gcc g++ linux-headers* libgcc*-dev *qt*
 apt-get autoremove -y
 
 rm -rf /var/lib/apt/lists/*
