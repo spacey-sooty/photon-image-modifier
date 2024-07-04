e76c6d880f0ddf2ca82e981ae12765c84771dcd8
Cameron (3539) <theforgelover@gmail.com>
2024-07-04 14:44:36 -0400

    Bump orangepi versions to 24.04 (#16)
    * Bump orangepi versions to 24.04
    
    Fixes https://github.com/PhotonVision/photonvision/issues/1307
    
    * oops
    
    * double oops...
    
    there are to many to pick from!
    
    * Add libc6 and libstdc++6
    
    * Latest version from Joshua Riek
    
    * Comment out images that aren't needed for testing the OPi build
    
    * enable all images
    
    * update checkout runner
    
    * master was a very old  branch
    
    * pin all runners to specific versions to avoid reversions and breaking changes
    
    * typo in one runner
    
    * try to fix OPi errors in arm-runner-action
    
    * add jammy to list of sources on 24.04
    
    * network management is caussing boot problems
    
    * don't remove snapd until the snaps are removed
    
    * better test for chroot
    
    * if syntax
    
    * try add-apt-repository
    
    * forgot main
    
    * put it at the end
    
    * no need to write the file
    
    ---------
    
    Co-authored-by: Craig Schardt <crschardt@fastem.com>

--- install_opi5.sh
+++ install_opi5.sh
@@ -17,27 +17,39 @@ chmod +x install.sh
 
 sed -i 's/# AllowedCPUs=4-7/AllowedCPUs=4-7/g' install.sh
 
-./install.sh
+./install.sh -n -q
 rm install.sh
 
 
 # Remove extra packages 
 echo "Purging extra things"
-apt-get remove -y gdb gcc g++ linux-headers* libgcc*-dev
-apt-get remove -y snapd
+# apt-get remove -y gdb gcc g++ linux-headers* libgcc*-dev
+# apt-get remove -y snapd
 apt-get autoremove -y
 
 
 echo "Installing additional things"
 sudo apt-get update
 apt-get install -y network-manager net-tools libatomic1
+
+apt-get install -y libc6 libstdc++6
+
+# cat > /etc/netplan/00-default-nm-renderer.yaml <<EOF
+# network:
+#   renderer: NetworkManager
+# EOF
+
+if [ $(cat /etc/lsb-release | grep -c "24.04") -gt 0 ]; then
+    # add jammy to apt sources 
+    echo "Adding jammy to list of apt sources"
+    add-apt-repository -y -S 'deb http://ports.ubuntu.com/ubuntu-ports jammy main universe'
+fi
+
+apt-get update
+
 # mrcal stuff
 apt-get install -y libcholmod3 liblapack3 libsuitesparseconfig5
 
-cat > /etc/netplan/00-default-nm-renderer.yaml <<EOF
-network:
-  renderer: NetworkManager
-EOF
 
 rm -rf /var/lib/apt/lists/*
 apt-get clean
