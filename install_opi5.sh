fbaa80270894a35f6eff2009daeced6220e060a5
Craig Schardt <crschardt@fastem.com>
2024-07-29 19:19:07 -0500

    Improvements for the OPi image (#19)
    Improve network configuration and boot behavior for the OrangePi5 Ubuntu 24.04 image and add support for the OrangePi5 Pro.

--- install_opi5.sh
+++ install_opi5.sh
@@ -4,43 +4,76 @@ if id "$1" >/dev/null 2>&1; then
     echo 'user found'
 else
     echo "creating pi user"
-    useradd pi -b /home
+    useradd pi -m -b /home -s /bin/bash
     usermod -a -G sudo pi
-    mkdir /home/pi
-    chown -R pi /home/pi
-
     echo 'pi ALL=(ALL) NOPASSWD: ALL' | tee -a /etc/sudoers.d/010_pi-nopasswd >/dev/null
     chmod 0440 /etc/sudoers.d/010_pi-nopasswd
 fi
 echo "pi:raspberry" | chpasswd
 
-apt-get update
+apt-get update --quiet
+
+before=$(df --output=used / | tail -n1)
+# clean up stuff
+echo 'Purging snaps'
+# get rid of snaps
+rm -rf /var/lib/snapd/seed/snaps/*
+rm -f /var/lib/snapd/seed/seed.yaml
+apt-get purge --yes --quiet lxd-installer lxd-agent-loader
+apt-get purge --yes --quiet snapd
+
+# remove bluetooth daemon
+apt-get purge --yes --quiet bluez
+
+apt-get --yes --quiet autoremove
+
+after=$(df --output=used / | tail -n1)
+freed=$(( before - after ))
+
+echo "Freed up $freed KiB"
+
+# run Photonvision install script
 wget https://git.io/JJrEP -O install.sh
 chmod +x install.sh
 
-sed -i 's/# AllowedCPUs=4-7/AllowedCPUs=4-7/g' install.sh
+sed -i 's/# AllowedCPUs=4-7/AllowedCPUs=0-7/g' install.sh
 
-./install.sh -n -q
+./install.sh -m -q
 rm install.sh
 
-
-# Remove extra packages 
-echo "Purging extra things"
-# apt-get remove -y gdb gcc g++ linux-headers* libgcc*-dev
-# apt-get remove -y snapd
-apt-get autoremove -y
-
-
 echo "Installing additional things"
-sudo apt-get update
-apt-get install -y network-manager net-tools libatomic1
 
-apt-get install -y libc6 libstdc++6
+apt-get install --yes --quiet network-manager net-tools libatomic1
 
-# cat > /etc/netplan/00-default-nm-renderer.yaml <<EOF
-# network:
-#   renderer: NetworkManager
+# let netplan create the config during cloud-init
+rm -f /etc/netplan/00-default-nm-renderer.yaml
+
+# set NetworkManager as the renderer in cloud-init
+cp -f ./OPi5_CIDATA/network-config /boot/network-config
+
+# add customized user-data file for cloud-init
+cp -f ./OPi5_CIDATA/user-data /boot/user-data
+
+# tell NetworkManager not to wait for the carrier on ethernet, which can delay boot
+# when the coprocessor isn't connected to the ethernet
+# cat > /etc/NetworkManager/conf.d/50-ignore-carrier.conf <<EOF
+# [main]
+# ignore-carrier=*
 # EOF
+
+# modify photonvision.service to wait for the network before starting
+# this helps ensure that photonvision detects the network the first time it starts
+# but it may cause a startup delay if the coprocessor isn't connected to a network
+sed -i '/Description/aAfter=network-online.target' /etc/systemd/system/photonvision.service
+cat /etc/systemd/system/photonvision.service
+
+# networkd isn't being used, this causes an unnecessary delay
+systemctl disable systemd-networkd-wait-online.service
+
+# the bluetooth service isn't needed and causes a delay at boot
+systemctl disable ap6275p-bluetooth.service
+
+apt-get install --yes --quiet libc6 libstdc++6
 
 if [ $(cat /etc/lsb-release | grep -c "24.04") -gt 0 ]; then
     # add jammy to apt sources 
@@ -48,14 +81,14 @@ if [ $(cat /etc/lsb-release | grep -c "24.04") -gt 0 ]; then
     add-apt-repository -y -S 'deb http://ports.ubuntu.com/ubuntu-ports jammy main universe'
 fi
 
-apt-get update
+apt-get --quiet update
 
 # mrcal stuff
-apt-get install -y libcholmod3 liblapack3 libsuitesparseconfig5
+apt-get install --yes --quiet libcholmod3 liblapack3 libsuitesparseconfig5
 
 
 rm -rf /var/lib/apt/lists/*
-apt-get clean
+apt-get --yes --quiet clean
 
 rm -rf /usr/share/doc
 rm -rf /usr/share/locale/
