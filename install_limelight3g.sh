0bbdf4148da71376adb5908f0c25a870931eb96c
Cameron (3539) <theforgelover@gmail.com>
2025-01-13 15:20:41 -0500

    Add LL3G Support (#32)
    Co-authored-by: Matt Morley <matthew.morley.ca@gmail.com>

---
+++ install_limelight3g.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -v
+
+# Verbose and exit on errors
+set -ex
+
+# Run the pi install script
+chmod +x ./install_pi.sh
+./install_pi.sh
+
+# Install our new config.txt with OV9281 overlay
+install -m 644 limelight3g/config.txt /boot/
+
+# Add the one extra file for the LL3
+wget https://datasheets.raspberrypi.org/cmio/dt-blob-cam1.bin -O /boot/dt-blob.bin
