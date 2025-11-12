1c9c752174b2304a0b2f1274c47542041294a095
Jade Turner <spacey-sooty@proton.me>
2025-11-12 22:09:20 +0800

    hello
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- install_limelight4.sh
+++ install_limelight4.sh
@@ -8,6 +8,7 @@ chmod +x ./install_pi.sh
 ./install_pi.sh
 
 # I want these for testing
+sudo apt install python3
 pip install smbus2 --break-system-packages
 
 # Install our new config.txt with OV9281 overlay
