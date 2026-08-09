8ce148cff991b7885865b1f0bea41f9dc8e14e2e
Craig Schardt <crschardt@fastem.com>
2026-08-09 15:51:57 -0500

    Switch to Armbian images for all OrangePi5 boards (#155)
    The OrangePi5 (& Rock-5C) images we have been using come from a repo
    that is no longer being maintained. This PR replaces those images with
    new Armbian images that are built in the
    PhotonVision/opi-image-generator repo.
    
    Benefits:
    - Uses updated images based on Debian Trixie
    - Provides path to updates to future images since Armbian is actively
    maintained
    - Images in opi-image-generator are tied to fixed releases to enable
    reproducible builds
    - OS startup times (as reported by `systemd-analyze`) of less than 10 s.
    - Complete reboot times, from shutdown to connected to NT and sending
    data, of less than 30 seconds.
    
    Notes about difference with the previous images:
    1. The `install_opi5_armbian.sh` script configures the images to
    completely avoid the armbian_firstlogin experience. It's intended for
    ease of configuration for interactive login, but I found that it was
    changing configuration and breaking the password set on the photon
    account.
    2. Wireless adaptors are blacklisted rather than soft blocked with
    rfkill.
    3. As described in #145, there are some cases where boot can take up to
    2.5 minutes. I've observed this on older OPi5 boards. The script adds
    extra kernel args meant to mitigate this problem.

--- install_opi5_armbian.sh
+++ install_opi5_armbian.sh
@@ -20,8 +20,8 @@ apt-get --yes -qq install libc6 libstdc++6
 # this adds `strings` so that users can check the version of U-Boot with `sudo strings /dev/mtd0 | grep "^U-Boot"``
 apt-get --yes -qq install binutils
 
-# copy configuration directives for first boot
-cp -f ./armbian/.not_logged_in_yet /root/
+# this isn't required, but is useful for monitoring temperature from the command line
+apt-get --yes -qq install  lm-sensors
 
 # modify photonvision.service to enable big cores
 sed -i 's/# AllowedCPUs=4-7/AllowedCPUs=4-7/g' /lib/systemd/system/photonvision.service
@@ -29,8 +29,8 @@ cp -f /lib/systemd/system/photonvision.service /etc/systemd/system/photonvision.
 chmod 644 /etc/systemd/system/photonvision.service
 cat /etc/systemd/system/photonvision.service
 
-# diagnose slow boot on Armbian images
-sed -i s/verbosity=1/verbosity=7/g /boot/armbianEnv.txt
+# try to 'fix' slow boot on Armbian images
+# sed -i s/verbosity=1/verbosity=7/g /boot/armbianEnv.txt
 sed -i 's/extraargs=/&initcall_debug ignore_loglevel cryptomgr.notests=1 nokprobes initcall_blacklist=init_kprobe_trace,crypto_kdf108_init,init_blk_tracer trace_buf_size=1 /' /boot/armbianEnv.txt
 
 # networkd isn't being used, this causes an unnecessary delay
@@ -39,33 +39,46 @@ sed -i 's/extraargs=/&initcall_debug ignore_loglevel cryptomgr.notests=1 nokprob
 # PhotonVision server is managing the network, so it doesn't need to wait for online
 systemctl disable NetworkManager-wait-online.service
 
-# the bluetooth service isn't needed and causes problems with cloud-init
-# the chip has different names on different boards. Examples are:
-#   OrangePi5: ap6275p-bluetooth.service
-#   OrangePi5pro: ap6256s-bluetooth.service
-#   OrangePi5b: ap6275p-bluetooth.service
-#   OrangePi5max: ap6611s-bluetooth.service
-# instead of keeping a catalog of these services, find them based on a pattern and mask them
-btservices=$(systemctl list-unit-files *bluetooth.service | tail -n +2 | head -n -1 | awk '{print $1}')
-for btservice in $btservices; do
-    echo "Masking: $btservice"
-    systemctl mask "$btservice"
-done
-
-# disable radios on first boot
-cat > /root/provisioning.sh << EOF
-#!/bin/bash
-hostnamectl set-hostname photonvision
-# disable radios on first boot
-echo "Running provisioning script" >> /root/provisioning.log
-nmcli radio all off
+# disable wireless by blacklisting broadcom drivers
+cat > /etc/modprobe.d/disable-wireless.conf << EOF
+# Disable wireless drivers to prevent them from loading
+blacklist brcmfmac
+blacklist brcmutil
+blacklist rtw88_core
+blacklist rtw88_pci
+blacklist rtw89_core
+blacklist rtw89_pci
 EOF
-chmod +x /root/provisioning.sh
+
+# mask the bluetooth and wpa services to preven them from being started
+systemctl mask bluetooth.service
+systemctl mask wpa_supplicant.service
+
+# disable NetworkManager from managing wireless interfaces
+mkdir -p /etc/NetworkManager/conf.d
+echo -e "[keyfile]\nunmanaged-devices=interface-name:wlan*" > /etc/NetworkManager/conf.d/disable-wifi.conf
+
 
 # set the hostname
 echo "photonvision" > /etc/hostname
 sed -i "s/127.0.1.1.*/127.0.1.1    photonvision/g" /etc/hosts
 
+# Prevent the firstlogin script from running when root logs in for the first time.
+# The script changes settings and overrides the photon user password.
+# Remove the sentinel file
+rm -f /root/.not_logged_in_yet
+
+# Erase the firstlogin script
+sudo rm -f /usr/lib/armbian/armbian-firstlogin
+
+# Create a blank, dummy script in its place to prevent "file not found" profile errors
+printf '#!/bin/bash\necho "First login script disabled"\nexit 0\n' > /usr/lib/armbian/armbian-firstlogin
+sudo chmod +x /usr/lib/armbian/armbian-firstlogin
+
+# disable the Armbian motd sripts
+chmod -x /etc/update-motd.d/*
+
+# Clean up apt cache and remove unnecessary files to reduce image size
 rm -rf /var/lib/apt/lists/*
 apt-get --yes -qq clean
 
